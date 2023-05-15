dad3b6a4df37c19b5034029f499a47ac3780b796
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-05-15 00:44:47 -0400

    add print function and rules about quotes

---
+++ CodeNotepad.py
@@ -0,0 +1,17 @@
+#Started: May 14,2023
+
+#To run the code in python line, press **** shift + enter ****, to run the code in python file, press **** F5 ****
+
+#Everytime that you want to find out about an error you are getting, you can copy and search it in google.
+
+print("Hello World!") #whole line is a statement. print is a function. Hello World! is an argument. we can have multiple arguments in a function. we call it a code block.
+
+#It will print everything in the brackets.
+#string is a sequence of characters(like a text), it is always in quotes.
+#we can use single quotes or double quotes. but if we have a single quote in the string, we have to use double quotes and vice versa. Also we can use triple quotes.
+
+print('Hello World! \n Hello World!')
+# \n is used to print the next line.
+
+print('Hello' + ' ' + 'Ehsan')
+      
