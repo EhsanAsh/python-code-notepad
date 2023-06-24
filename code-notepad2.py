b34669ad39804ed0619cfe00dc3bdbbdb696dbf8
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-06-24 01:31:15 -0400

    move funtion types to new file

--- code-notepad2.py
+++ code-notepad2.py
@@ -1,1 +1,13 @@
 #-------------------------------------Functions------------------------------------
+
+#--------Function types--------
+
+print() #it is a built-in function. we can use it to print something.
+input() #it is a built-in function. we can use it to get input from the user.
+len() #it is a built-in function. we can use it to count the number of characters in a string.
+type() #it is a built-in function. we can use it to find out the type of the variable.
+int() #it is a built-in function. we can use it to convert a number to an integer.
+float() #it is a built-in function. we can use it to convert a number to a float.
+str() #it is a built-in function. we can use it to convert a number to a string.
+help() #it is a built-in function. we can use it to find out how to use a specific method.
+round(thing to round , how many decimal points to rount it to) #it is a built-in function. we can use it to round a number.
