5d56d5ec14b59f567e3c5164f5d0a3728234b347
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-06-24 01:31:13 -0400

    move funtion types to new file

--- CodeNotepad.py
+++ CodeNotepad.py
@@ -303,18 +303,6 @@ print(name,length)
 name = input()
 #an empty input is a falsy value. here it will give the user a chance to enter the answer again or a cursor.
 
-#-------------------------------Function types------------------------------------------
-
-print() #it is a built-in function. we can use it to print something.
-input() #it is a built-in function. we can use it to get input from the user.
-len() #it is a built-in function. we can use it to count the number of characters in a string.
-type() #it is a built-in function. we can use it to find out the type of the variable.
-int() #it is a built-in function. we can use it to convert a number to an integer.
-float() #it is a built-in function. we can use it to convert a number to a float.
-str() #it is a built-in function. we can use it to convert a number to a string.
-help() #it is a built-in function. we can use it to find out how to use a specific method.
-round(thing to round , how many decimal points to rount it to) #it is a built-in function. we can use it to round a number.
-
 #-----------------------------------Conditional Statements-------------------------------
 
 #Conditional statements are used to make decisions. we can use if, elif, else to make decisions.
